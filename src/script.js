45679a2f483a66a05524650ed370449a3f7221fc
RafaelUeda <rafael@teknova.com.br>
2024-12-06 15:23:34 -0300

    removed quinta.js and improved js

--- src/script.js
+++ src/script.js
@@ -1,28 +1,26 @@
-// Função para calcular a porcentagem de progresso com base no tempo
 function atualizarProgresso() {
-    const inicioTrabalho = new Date();
-    inicioTrabalho.setHours(8, 0, 0); // 08:00
-
-    const fimTrabalho = new Date();
-    fimTrabalho.setHours(18, 0, 0); // 18:00
-
     const agora = new Date();
+    const inicioTrabalho = new Date(agora);
+    inicioTrabalho.setHours(8, 0, 0, 0);
 
-    // Se a hora atual estiver fora do horário de trabalho, ajusta o progresso
+    const fimTrabalho = new Date(agora);
+    fimTrabalho.setHours(18, 0, 0, 0);
+
     let progresso = 0;
-    if (agora < inicioTrabalho) {
-        progresso = 0;
-    } else if (agora > fimTrabalho) {
-        progresso = 100;
-    } else {
+    if (agora >= inicioTrabalho && agora <= fimTrabalho) {
         const totalSegundosDia = (fimTrabalho - inicioTrabalho) / 1000;
         const segundosPassados = (agora - inicioTrabalho) / 1000;
         progresso = (segundosPassados / totalSegundosDia) * 100;
+    } else if (agora > fimTrabalho) {
+        progresso = 100;
     }
 
-    document.getElementById('progress-bar').style.width = progresso + '%';
-    document.getElementById('progress-percentage').textContent = Math.floor(progresso) + '%';
+    const progressBar = document.getElementById('progress-bar');
+    const progressPercentage = document.getElementById('progress-percentage');
+    
+    progressBar.style.width = `${progresso}%`;
+    progressPercentage.textContent = `${Math.floor(progresso)}%`;
 }
 
-// Atualiza a barra a cada segundo
 setInterval(atualizarProgresso, 1000);
+
